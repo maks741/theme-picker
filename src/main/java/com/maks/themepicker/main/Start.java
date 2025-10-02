259a945c287d16372832780951636f81f0643cb5
Maksym Kozak <bersercer70@gmail.com>
2025-10-02 14:27:00 +0300

    first iteration of optimizations

--- src/main/java/com/maks/themepicker/main/Start.java
+++ src/main/java/com/maks/themepicker/main/Start.java
@@ -4,17 +4,12 @@ import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -23,19 +18,19 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.util.Duration;
 
-import javax.swing.*;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Start extends Application {
     private int selectedIndex = 0;
-    private final List<ImageView> imageViews = new ArrayList<>();
     private final List<Rectangle> clips = new ArrayList<>();
     private final List<ColorAdjust> effects = new ArrayList<>();
-    private boolean init = true;
+    private final Duration animationDuration = Duration.millis(100);
 
     Screen screen = Screen.getPrimary();
     double screenWidth = screen.getBounds().getWidth();
@@ -48,27 +43,49 @@ public class Start extends Application {
 
     @Override
     public void start(Stage stage) {
+        Path currentWallpaperPath = Paths.get(System.getProperty("user.home"), ".config", "wallpapers", "current");
+        ImageView currentWallpaper = new ImageView(new Image(currentWallpaperPath.toUri().toString(), 640, 360, false, true));
+        currentWallpaper.setFitWidth(screenWidth);
+        currentWallpaper.setFitHeight(screenHeight);
+        ColorAdjust dim = new ColorAdjust();
+        dim.setBrightness(-0.75);
+        currentWallpaper.setEffect(dim);
 
-        HBox box = new HBox();
+        StackPane root = new StackPane();
+        root.setAlignment(Pos.CENTER);
+        root.getChildren().add(currentWallpaper);
 
-        Scene scene = new Scene(box);
+        Scene scene = new Scene(root);
         scene.setFill(Color.rgb(30, 30, 30, 0.3));
 
-        box.setSpacing(10);
-        box.setPrefWidth(screenWidth);
-        box.setPrefHeight(screenHeight);
-        box.setAlignment(Pos.CENTER);
+        stage.setTitle("theme-picker");
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setScene(scene);
+        stage.setX(0);
+        stage.setY(0);
+        stage.show();
 
-        Path[] paths = {
-                Paths.get(System.getProperty("user.home"), ".config", "themes", "anime_girl", "wallpaper"),
-                Paths.get(System.getProperty("user.home"), ".config", "themes", "katana", "wallpaper"),
-                Paths.get(System.getProperty("user.home"), ".config", "themes", "ruins", "wallpaper"),
-                Paths.get(System.getProperty("user.home"), ".config", "themes", "hyprland_kath", "wallpaper"),
-                Paths.get(System.getProperty("user.home"), ".config", "themes", "mountain", "wallpaper"),
-        };
+        HBox hBox = new HBox();
+        hBox.setPrefWidth(screenWidth);
+        hBox.setPrefHeight(screenHeight);
+
+        root.getChildren().add(hBox);
+
+        hBox.setSpacing(10);
+        hBox.setPrefWidth(screenWidth);
+        hBox.setPrefHeight(screenHeight);
+        hBox.setAlignment(Pos.CENTER);
+
+        List<Path> paths = new ArrayList<>();
+        try (Stream<Path> themeDirs = Files.list(Paths.get(System.getProperty("user.home"), ".config", "themes"))) {
+            themeDirs.map(themeDir -> themeDir.resolve("wallpaper"))
+                    .forEach(paths::add);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         for (Path path : paths) {
-            ImageView iv = new ImageView(new Image(path.toUri().toString()));
+            ImageView iv = new ImageView(new Image(path.toUri().toString(), 1280, 720, false, true, true));
             iv.setFitHeight(imageHeight);
             iv.setPreserveRatio(true);
 
@@ -85,60 +102,47 @@ public class Start extends Application {
             wrapper.prefWidthProperty().bind(clip.widthProperty());
             wrapper.maxWidthProperty().bind(clip.widthProperty());
 
-            imageViews.add(iv);
             clips.add(clip);
             effects.add(effect);
 
-            box.getChildren().add(wrapper);
+            hBox.getChildren().add(wrapper);
         }
 
         scene.setOnKeyPressed(e -> {
-            if (e.getCode() == KeyCode.RIGHT) {
-                if (selectedIndex < imageViews.size() - 1) {
-                    updateSelection(selectedIndex + 1);
+            switch (e.getCode()) {
+                case RIGHT -> {
+                    if (selectedIndex < clips.size() - 1) {
+                        updateSelection(selectedIndex + 1);
+                    }
                 }
-            } else if (e.getCode() == KeyCode.LEFT) {
-                if (selectedIndex > 0) {
-                    updateSelection(selectedIndex - 1);
+                case LEFT -> {
+                    if (selectedIndex > 0) {
+                        updateSelection(selectedIndex - 1);
+                    }
                 }
+                case ESCAPE -> exit();
             }
         });
 
-        updateSelection(0);
-
-        stage.setTitle("theme-picker");
-        stage.initStyle(StageStyle.TRANSPARENT);
-        stage.setX(0);
-        stage.setY(0);
-        stage.setScene(scene);
-        stage.show();
+        instantSelectFirst();
     }
 
     private void updateSelection(int newIndex) {
-        if (newIndex == selectedIndex && !init) {
-            return;
-        }
-        init = false;
+        deselectOld();
+        selectNew(newIndex);
 
-        // animate deselection of old
-        Rectangle oldClip = clips.get(selectedIndex);
-        ColorAdjust oldEffect = effects.get(selectedIndex);
+        selectedIndex = newIndex;
+    }
 
-        Timeline oldAnim = new Timeline(
-                new KeyFrame(Duration.ZERO,
-                        new KeyValue(oldClip.widthProperty(), oldClip.getWidth()),
-                        new KeyValue(oldClip.xProperty(), oldClip.getX()),
-                        new KeyValue(oldEffect.brightnessProperty(), oldEffect.getBrightness())
-                ),
-                new KeyFrame(Duration.seconds(0.1),
-                        new KeyValue(oldClip.widthProperty(), croppedImageWidth),
-                        new KeyValue(oldClip.xProperty(), (imageWidth - croppedImageWidth) / 2.0),
-                        new KeyValue(oldEffect.brightnessProperty(), -0.5)
-                )
-        );
-        oldAnim.play();
+    private void instantSelectFirst() {
+        selectNew(0, Duration.millis(1));
+    }
 
-        // animate selection of new
+    private void selectNew(int newIndex) {
+        selectNew(newIndex, animationDuration);
+    }
+
+    private void selectNew(int newIndex, Duration animationDuration) {
         Rectangle newClip = clips.get(newIndex);
         ColorAdjust newEffect = effects.get(newIndex);
 
@@ -148,14 +152,35 @@ public class Start extends Application {
                         new KeyValue(newClip.xProperty(), newClip.getX()),
                         new KeyValue(newEffect.brightnessProperty(), newEffect.getBrightness())
                 ),
-                new KeyFrame(Duration.seconds(0.1),
+                new KeyFrame(animationDuration,
                         new KeyValue(newClip.widthProperty(), selectedImageWidth),
                         new KeyValue(newClip.xProperty(), (imageWidth - selectedImageWidth) / 2.0),
                         new KeyValue(newEffect.brightnessProperty(), 0)
                 )
         );
         newAnim.play();
+    }
 
-        selectedIndex = newIndex;
+    private void deselectOld() {
+        Rectangle oldClip = clips.get(selectedIndex);
+        ColorAdjust oldEffect = effects.get(selectedIndex);
+
+        Timeline oldAnim = new Timeline(
+                new KeyFrame(Duration.ZERO,
+                        new KeyValue(oldClip.widthProperty(), oldClip.getWidth()),
+                        new KeyValue(oldClip.xProperty(), oldClip.getX()),
+                        new KeyValue(oldEffect.brightnessProperty(), oldEffect.getBrightness())
+                ),
+                new KeyFrame(animationDuration,
+                        new KeyValue(oldClip.widthProperty(), croppedImageWidth),
+                        new KeyValue(oldClip.xProperty(), (imageWidth - croppedImageWidth) / 2.0),
+                        new KeyValue(oldEffect.brightnessProperty(), -0.5)
+                )
+        );
+        oldAnim.play();
+    }
+
+    private void exit() {
+        System.exit(0);
     }
 }
